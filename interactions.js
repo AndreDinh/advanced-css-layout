4770d333113723173d0d65aa9b85d7bdc187ea39
Andre Dinh <dinh.andre@yahoo.com>
2015-06-22 19:13:30 -0700

    added javascript

--- interactions.js
+++ interactions.js
@@ -1,9 +1,53 @@
-$(window).scroll(function () {
-  if ( $(this).scrollTop() > 200 && !$('header').hasClass('open') ) {
-    $('header').addClass('open');
-    $('header').slideDown();
-   } else if ( $(this).scrollTop() <= 200 ) {
-    $('header').removeClass('open');
-    $('header').slideUp();
-  }
-});
+// var imageParagraph = document.getElementById('imgP');
+// imageParagraph.addclass('animated bounceIn');
+// var fieldParagraph = document.getElementById('fieldP');
+// fieldParagraph.addclass('animated bounceIn');
+// var paragraph = document.getElementById('paragraph');
+// paragraph.addclass('animated flipInY')
+var hover = (function()
+{
+    function mouseMoveF(e)
+    {
+        if(!this.shel && this.classList.contains('hidden'))
+        {
+            this.classList.remove('hidden');
+            return;
+        }
+
+        if(this.shel.classList.contains('hidden'))
+        {
+            this.shel.classList.remove('hidden');
+            return;
+        }
+    }
+
+    function mouseOutF(e)
+    {
+        if(!this.shel)
+        {
+            this.classList.add('hidden');
+            return;
+        }
+
+        this.shel.classList.add('hidden');
+    }
+
+    function moveShow(el, shel)
+    {
+        el.shel = shel;
+
+        el.addEventListener('mousemove', mouseMoveF);
+        el.addEventListener('mouseout', mouseOutF);
+
+        shel.addEventListener('mousemove', mouseMoveF);
+        shel.addEventListener('mouseout', mouseOutF);
+
+        shel.classList.add('hidden');
+    }
+
+    return {
+        moveShow: moveShow,
+        mouseOutF: mouseOutF,
+        mouseMoveF: mouseMoveF
+    };
+})();
